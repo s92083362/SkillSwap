d45f6cc50e0d49f40eeb29bada5db1e93d8d5458
s92083362 <s92083362@ousl.lk>
2025-11-10 15:23:52 +0530

    feat: add profile route with page.tsx

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+// src/app/profile/page.tsx
+import ProfilePage from "../../components/Profile/ProfilePage";
+export default function ProfileRoute() {
+  return (
+    <main>
+      <ProfilePage />
+    </main>
+  );
+}
